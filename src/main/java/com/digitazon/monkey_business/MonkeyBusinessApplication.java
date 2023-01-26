795cac05ba18a4561b489c53333ef767859b0a1a
hobisognodiunarma <alessandroarmari22@gmail.com>
2023-01-26 14:33:55 +0100

    first commit

---
+++ src/main/java/com/digitazon/monkey_business/MonkeyBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.digitazon.monkey_business;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonkeyBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonkeyBusinessApplication.class, args);
+	}
+
+}
